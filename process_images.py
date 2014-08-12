4190f793f45dd61b6e9c273a736ff0ec92fd0d44
EricSchles <ericschles@gmail.com>
2014-08-12 11:49:56 -0700

    works mostly

---
+++ process_images.py
@@ -0,0 +1,13 @@
+from FaceDetect.face_detect import run
+import glob
+import Image
+import os
+images = glob.glob("preprocessed/*")
+
+for image in images:
+    png_img = image.split(".")[0] + ".png"
+    im = Image.open(image)
+    im.save(png_img)
+    os.remove(image)
+    print png_img
+    run(png_img)
