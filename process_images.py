449ed55000e2ac1c32e0a4726bf9d5ae33b181d8
EricSchles <ericschles@gmail.com>
2014-08-12 12:05:15 -0700

    everything works now

--- process_images.py
+++ process_images.py
@@ -9,6 +9,6 @@ for image in images:
     png_img = image.split(".")[0] + ".png"
     im = Image.open(image)
     im.save(png_img)
-    os.remove(image)
     print png_img
     run(png_img)
+    os.remove(image)
